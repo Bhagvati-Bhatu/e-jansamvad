e203cfaa8519f0377deb3811965dcfa0fc8e77c7
Bhagvati-Bhatu <bhagvatibhatu90@gmail.com>
2025-10-14 21:07:46 +0530

    resolving bugs

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,10 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cookieParser());
 app.use(cors({
-    origin: "https://aspire-hackathon-one.vercel.app",// Allow frontend requests
+    origin: "https://e-jansamvad.onrender.com",// Allow frontend requests
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     optionsSuccessStatus: 200
 }));
 app.use(express.json());
